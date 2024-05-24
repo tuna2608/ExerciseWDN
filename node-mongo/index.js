dc47f777f8dd6f7f23c63de690e4736f0f32ca37
tuna2608 <tuna1.dev@gmail.com>
2024-05-24 16:33:51 +0700

    Node Mongo Ex 2

--- node-mongo/index.js
+++ node-mongo/index.js
@@ -3,33 +3,36 @@ const assert = require('assert');
 
 const url = 'mongodb://localhost:27017/';
 const dbname = 'conFusion';
+const dboper = require('./operations');
 
 MongoClient.connect(url, (err, client) => {
-
     assert.equal(err, null);
-
     console.log('Connected correctly to server');
 
     const db = client.db(dbname);
     const collection = db.collection("dishes");
-    collection.insertOne({ "name": "Uthappizza1", "description": "test1" },
-        (err, result) => {
-            assert.equal(err, null);
+    dboper.insertDocument(db, { name: "Vadonut", description: "Test"},
+        "dishes", (result) => {
+            console.log("Insert Document:\n", result.ops);
 
-            console.log("After Insert:\n");
-            console.log(result.ops);
+            dboper.findDocuments(db, "dishes", (docs) => {
+                console.log("Found Documents:\n", docs);
 
-            collection.find({}).toArray((err, docs) => {
-                assert.equal(err, null);
+                dboper.updateDocument(db, { name: "Vadonut" },
+                    { description: "Updated Test" }, "dishes",
+                    (result) => {
+                        console.log("Updated Document:\n", result.result);
 
-                console.log("Found:\n");
-                console.log(docs);
+                        dboper.findDocuments(db, "dishes", (docs) => {
+                            console.log("Found Updated Documents:\n", docs);
+                            
+                            // db.dropCollection("dishes", (result) => {
+                            //     console.log("Dropped Collection: ", result);
 
-                // db.dropCollection("dishes", (err, result) => {
-                //     assert.equal(err, null);
-
-                //     client.close();
-                // });
+                            //     client.close();
+                            // });
+                        });
+                    });
             });
-        });
+    });
 });
